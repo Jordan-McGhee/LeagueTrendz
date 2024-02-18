6b12bcc8623977bc1ffcf7d75b28769ad3366a4f
Jordan McGhee <jordanmcghee95@gmail.com>
2024-02-18 13:23:31 -0500

    Set up divs for next game div in player overview

--- frontend/src/components/Desktop/PlayerPage/Overview/NextGame.tsx
+++ frontend/src/components/Desktop/PlayerPage/Overview/NextGame.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 // ui imports
 import { Card, CardHeader, CardTitle, CardContent } from "../../../ui/card"
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "../../../ui/table"
 
 const NextGame = () => {
 
@@ -15,6 +16,18 @@ const NextGame = () => {
                     </div>
                 </CardTitle>
             </CardHeader>
+
+            <CardContent>
+
+                <div>
+                    {/* next team data/time div */}
+
+                    {/* stats vs that team div */}
+
+                    {/* table for splits */}
+                </div>
+                
+            </CardContent>
         </Card>
     )
 }
