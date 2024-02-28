971741319bfb7122bb0e823fe4c2577556509d0a
Jordan McGhee <jordanmcghee95@gmail.com>
2024-02-27 20:59:23 -0500

    Small change to playerbio

--- frontend/src/pages/Data-Pages/Player/Views/PlayerBio.tsx
+++ frontend/src/pages/Data-Pages/Player/Views/PlayerBio.tsx
@@ -10,7 +10,7 @@ const PlayerBio = () => {
 
     return (
         <div className="flex justify-between gap-x-4 h-fit mt-4">
-            
+
             {/* left side */}
             <div className="h-screen w-[65%] flex flex-col gap-y-4">
                 <PlayerBiography />
