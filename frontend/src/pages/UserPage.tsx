5733d652c92ca7758e9783e0e55215ed6e73dad7
Jordan McGhee <jordanmcghee95@gmail.com>
2024-03-05 17:07:15 -0500

    Starting on user page on front end

---
+++ frontend/src/pages/UserPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// ui imports
+import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "../components/ui/card"
+
+const UserPage = () => {
+
+    return (
+        <div></div>
+    )
+}
+
+export default UserPage
