2293e2c9ab2c27bd29716ddb0d2290b9bca474e9
Jordan McGhee <jordanmcghee95@gmail.com>
2024-04-27 16:57:50 -0400

    Added table for player splits

--- frontend/src/components/Desktop/PlayerPage/Splits/PlayerSplitsTable.tsx
+++ frontend/src/components/Desktop/PlayerPage/Splits/PlayerSplitsTable.tsx
@@ -6,9 +6,6 @@ import { Table, TableHeader, TableRow, TableCell, TableHead, TableBody } from ".
 
 const PlayerSplitsTable: React.FC<SplitsTableProps> = ({ data }) => {
 
-    // const { overall_averages, home_averages, away_averages, conference_averages, division_averages, win_averages, loss_averages } = data
-    // const averages = [overall_averages, home_averages, away_averages, conference_averages, division_averages, win_averages, loss_averages]
-
     return (
         <Table className="text-xs mb-2">
             <TableHeader>
@@ -32,7 +29,147 @@ const PlayerSplitsTable: React.FC<SplitsTableProps> = ({ data }) => {
                 </TableRow>
             </TableHeader>
             <TableBody>
-                
+
+                {/* overall */}
+                <TableRow key='split_overall'>
+                    <TableCell>All Splits</TableCell>
+                    <TableCell>{data?.overall_averages.gp}</TableCell>
+                    <TableCell className="text-center">{data?.overall_averages.avg_minutes}</TableCell>
+                    <TableCell className="text-center">{data?.overall_averages.avg_pts}</TableCell>
+                    <TableCell className="text-center">{data?.overall_averages.avg_fgm}-{data?.overall_averages.avg_fga}</TableCell>
+                    <TableCell className="text-center">{data?.overall_averages.avg_fg_percentage}</TableCell>
+                    <TableCell className="text-center">{data?.overall_averages.avg_tpm}-{data?.overall_averages.avg_tpa}</TableCell>
+                    <TableCell className="text-center">{data?.overall_averages.avg_tp_percentage}</TableCell>
+                    <TableCell className="text-center">{data?.overall_averages.avg_ftm}-{data?.overall_averages.avg_fta}</TableCell>
+                    <TableCell className="text-center">{data?.overall_averages.avg_ft_percentage}</TableCell>
+                    <TableCell className="text-center">{data?.overall_averages.avg_reb}</TableCell>
+                    <TableCell className="text-center">{data?.overall_averages.avg_ast}</TableCell>
+                    <TableCell className="text-center">{data?.overall_averages.avg_blk}</TableCell>
+                    <TableCell className="text-center">{data?.overall_averages.avg_stl}</TableCell>
+                    <TableCell className="text-center">{data?.overall_averages.avg_pf}</TableCell>
+                    <TableCell className="text-center">{data?.overall_averages.avg_turnovers}</TableCell>
+                </TableRow>
+
+                {/* home */}
+                <TableRow key='split_overall'>
+                    <TableCell>Home</TableCell>
+                    <TableCell>{data?.home_averages.gp}</TableCell>
+                    <TableCell className="text-center">{data?.home_averages.avg_minutes}</TableCell>
+                    <TableCell className="text-center">{data?.home_averages.avg_pts}</TableCell>
+                    <TableCell className="text-center">{data?.home_averages.avg_fgm}-{data?.home_averages.avg_fga}</TableCell>
+                    <TableCell className="text-center">{data?.home_averages.avg_fg_percentage}</TableCell>
+                    <TableCell className="text-center">{data?.home_averages.avg_tpm}-{data?.home_averages.avg_tpa}</TableCell>
+                    <TableCell className="text-center">{data?.home_averages.avg_tp_percentage}</TableCell>
+                    <TableCell className="text-center">{data?.home_averages.avg_ftm}-{data?.home_averages.avg_fta}</TableCell>
+                    <TableCell className="text-center">{data?.home_averages.avg_ft_percentage}</TableCell>
+                    <TableCell className="text-center">{data?.home_averages.avg_reb}</TableCell>
+                    <TableCell className="text-center">{data?.home_averages.avg_ast}</TableCell>
+                    <TableCell className="text-center">{data?.home_averages.avg_blk}</TableCell>
+                    <TableCell className="text-center">{data?.home_averages.avg_stl}</TableCell>
+                    <TableCell className="text-center">{data?.home_averages.avg_pf}</TableCell>
+                    <TableCell className="text-center">{data?.home_averages.avg_turnovers}</TableCell>
+                </TableRow>
+
+                {/* away */}
+                <TableRow key='split_overall'>
+                    <TableCell>Away</TableCell>
+                    <TableCell>{data?.away_averages.gp}</TableCell>
+                    <TableCell className="text-center">{data?.away_averages.avg_minutes}</TableCell>
+                    <TableCell className="text-center">{data?.away_averages.avg_pts}</TableCell>
+                    <TableCell className="text-center">{data?.away_averages.avg_fgm}-{data?.away_averages.avg_fga}</TableCell>
+                    <TableCell className="text-center">{data?.away_averages.avg_fg_percentage}</TableCell>
+                    <TableCell className="text-center">{data?.away_averages.avg_tpm}-{data?.away_averages.avg_tpa}</TableCell>
+                    <TableCell className="text-center">{data?.away_averages.avg_tp_percentage}</TableCell>
+                    <TableCell className="text-center">{data?.away_averages.avg_ftm}-{data?.away_averages.avg_fta}</TableCell>
+                    <TableCell className="text-center">{data?.away_averages.avg_ft_percentage}</TableCell>
+                    <TableCell className="text-center">{data?.away_averages.avg_reb}</TableCell>
+                    <TableCell className="text-center">{data?.away_averages.avg_ast}</TableCell>
+                    <TableCell className="text-center">{data?.away_averages.avg_blk}</TableCell>
+                    <TableCell className="text-center">{data?.away_averages.avg_stl}</TableCell>
+                    <TableCell className="text-center">{data?.away_averages.avg_pf}</TableCell>
+                    <TableCell className="text-center">{data?.away_averages.avg_turnovers}</TableCell>
+                </TableRow>
+
+                {/* conference */}
+                <TableRow key='split_overall'>
+                    <TableCell>Conference</TableCell>
+                    <TableCell>{data?.conference_averages.gp}</TableCell>
+                    <TableCell className="text-center">{data?.conference_averages.avg_minutes}</TableCell>
+                    <TableCell className="text-center">{data?.conference_averages.avg_pts}</TableCell>
+                    <TableCell className="text-center">{data?.conference_averages.avg_fgm}-{data?.conference_averages.avg_fga}</TableCell>
+                    <TableCell className="text-center">{data?.conference_averages.avg_fg_percentage}</TableCell>
+                    <TableCell className="text-center">{data?.conference_averages.avg_tpm}-{data?.conference_averages.avg_tpa}</TableCell>
+                    <TableCell className="text-center">{data?.conference_averages.avg_tp_percentage}</TableCell>
+                    <TableCell className="text-center">{data?.conference_averages.avg_ftm}-{data?.conference_averages.avg_fta}</TableCell>
+                    <TableCell className="text-center">{data?.conference_averages.avg_ft_percentage}</TableCell>
+                    <TableCell className="text-center">{data?.conference_averages.avg_reb}</TableCell>
+                    <TableCell className="text-center">{data?.conference_averages.avg_ast}</TableCell>
+                    <TableCell className="text-center">{data?.conference_averages.avg_blk}</TableCell>
+                    <TableCell className="text-center">{data?.conference_averages.avg_stl}</TableCell>
+                    <TableCell className="text-center">{data?.conference_averages.avg_pf}</TableCell>
+                    <TableCell className="text-center">{data?.conference_averages.avg_turnovers}</TableCell>
+                </TableRow>
+
+                {/* division */}
+                <TableRow key='split_overall'>
+                    <TableCell>Division</TableCell>
+                    <TableCell>{data?.division_averages.gp}</TableCell>
+                    <TableCell className="text-center">{data?.division_averages.avg_minutes}</TableCell>
+                    <TableCell className="text-center">{data?.division_averages.avg_pts}</TableCell>
+                    <TableCell className="text-center">{data?.division_averages.avg_fgm}-{data?.division_averages.avg_fga}</TableCell>
+                    <TableCell className="text-center">{data?.division_averages.avg_fg_percentage}</TableCell>
+                    <TableCell className="text-center">{data?.division_averages.avg_tpm}-{data?.division_averages.avg_tpa}</TableCell>
+                    <TableCell className="text-center">{data?.division_averages.avg_tp_percentage}</TableCell>
+                    <TableCell className="text-center">{data?.division_averages.avg_ftm}-{data?.division_averages.avg_fta}</TableCell>
+                    <TableCell className="text-center">{data?.division_averages.avg_ft_percentage}</TableCell>
+                    <TableCell className="text-center">{data?.division_averages.avg_reb}</TableCell>
+                    <TableCell className="text-center">{data?.division_averages.avg_ast}</TableCell>
+                    <TableCell className="text-center">{data?.division_averages.avg_blk}</TableCell>
+                    <TableCell className="text-center">{data?.division_averages.avg_stl}</TableCell>
+                    <TableCell className="text-center">{data?.division_averages.avg_pf}</TableCell>
+                    <TableCell className="text-center">{data?.division_averages.avg_turnovers}</TableCell>
+                </TableRow>
+
+                {/* wins */}
+                <TableRow key='split_overall'>
+                    <TableCell>Wins</TableCell>
+                    <TableCell>{data?.win_averages.gp}</TableCell>
+                    <TableCell className="text-center">{data?.win_averages.avg_minutes}</TableCell>
+                    <TableCell className="text-center">{data?.win_averages.avg_pts}</TableCell>
+                    <TableCell className="text-center">{data?.win_averages.avg_fgm}-{data?.win_averages.avg_fga}</TableCell>
+                    <TableCell className="text-center">{data?.win_averages.avg_fg_percentage}</TableCell>
+                    <TableCell className="text-center">{data?.win_averages.avg_tpm}-{data?.win_averages.avg_tpa}</TableCell>
+                    <TableCell className="text-center">{data?.win_averages.avg_tp_percentage}</TableCell>
+                    <TableCell className="text-center">{data?.win_averages.avg_ftm}-{data?.win_averages.avg_fta}</TableCell>
+                    <TableCell className="text-center">{data?.win_averages.avg_ft_percentage}</TableCell>
+                    <TableCell className="text-center">{data?.win_averages.avg_reb}</TableCell>
+                    <TableCell className="text-center">{data?.win_averages.avg_ast}</TableCell>
+                    <TableCell className="text-center">{data?.win_averages.avg_blk}</TableCell>
+                    <TableCell className="text-center">{data?.win_averages.avg_stl}</TableCell>
+                    <TableCell className="text-center">{data?.win_averages.avg_pf}</TableCell>
+                    <TableCell className="text-center">{data?.win_averages.avg_turnovers}</TableCell>
+                </TableRow>
+
+                {/* losses */}
+                <TableRow key='split_overall'>
+                    <TableCell>Losses</TableCell>
+                    <TableCell>{data?.loss_averages.gp}</TableCell>
+                    <TableCell className="text-center">{data?.loss_averages.avg_minutes}</TableCell>
+                    <TableCell className="text-center">{data?.loss_averages.avg_pts}</TableCell>
+                    <TableCell className="text-center">{data?.loss_averages.avg_fgm}-{data?.loss_averages.avg_fga}</TableCell>
+                    <TableCell className="text-center">{data?.loss_averages.avg_fg_percentage}</TableCell>
+                    <TableCell className="text-center">{data?.loss_averages.avg_tpm}-{data?.loss_averages.avg_tpa}</TableCell>
+                    <TableCell className="text-center">{data?.loss_averages.avg_tp_percentage}</TableCell>
+                    <TableCell className="text-center">{data?.loss_averages.avg_ftm}-{data?.loss_averages.avg_fta}</TableCell>
+                    <TableCell className="text-center">{data?.loss_averages.avg_ft_percentage}</TableCell>
+                    <TableCell className="text-center">{data?.loss_averages.avg_reb}</TableCell>
+                    <TableCell className="text-center">{data?.loss_averages.avg_ast}</TableCell>
+                    <TableCell className="text-center">{data?.loss_averages.avg_blk}</TableCell>
+                    <TableCell className="text-center">{data?.loss_averages.avg_stl}</TableCell>
+                    <TableCell className="text-center">{data?.loss_averages.avg_pf}</TableCell>
+                    <TableCell className="text-center">{data?.loss_averages.avg_turnovers}</TableCell>
+                </TableRow>
+
             </TableBody>
         </Table>
     )
--- frontend/src/pages/Data-Pages/Player/Views/PlayerSplits.tsx
+++ frontend/src/pages/Data-Pages/Player/Views/PlayerSplits.tsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from "react"
 import { useFetch } from "../../../../Hooks/useFetch"
 
 // type imports
-import { PlayerPageProps, SplitsData} from "../../../../types"
+import { PlayerPageProps, SplitsData } from "../../../../types"
 
 // ui imports
 import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "../../../../components/ui/card"
@@ -47,52 +47,54 @@ const PlayerSplits: React.FC<PlayerPageProps> = ({ player, currentTeam }) => {
             {/* loading state */}
             {isLoading && <LoadingPage />}
 
-            <Card className="mt-4">
-                <CardHeader>
-                    <CardTitle>
-                        <p className="text-2xl">2023-24 Splits</p>
-                    </CardTitle>
-                </CardHeader>
+            {data &&
+                <Card className="mt-4">
+                    <CardHeader>
+                        <CardTitle>
+                            <p className="text-2xl">2023-24 Splits</p>
+                        </CardTitle>
+                    </CardHeader>
 
-                <CardContent className="flex flex-col gap-y-2">
-                    <PlayerSplitsTable data={data} />
-                </CardContent>
+                    <CardContent className="flex flex-col gap-y-2">
+                        <PlayerSplitsTable data={data} />
+                    </CardContent>
 
-                <CardFooter>
-                    <div className="w-full pt-2">
+                    <CardFooter>
+                        <div className="w-full pt-2">
 
-                        <p className="text-xs font-semibold">GLOSSARY:</p>
+                            <p className="text-xs font-semibold">GLOSSARY:</p>
 
-                        {/* GLOSSARY */}
-                        <div className="text-xs mt-4 flex justify-between">
+                            {/* GLOSSARY */}
+                            <div className="text-xs mt-4 flex justify-between">
 
-                            <div>
-                                <p><span className="font-bold">3P%:</span> 3-Point Field Goal Percentage</p>
-                                <p><span className="font-bold">3PA:</span> 3-Point Field Goals Made-Attempted</p>
-                                <p><span className="font-bold">AST:</span> Assists Per Game</p>
-                                <p><span className="font-bold">BLK:</span> Blocks Per Game</p>
-                                <p><span className="font-bold">FGA:</span> Field Goals Made-Attempted</p>
+                                <div>
+                                    <p><span className="font-bold">3P%:</span> 3-Point Field Goal Percentage</p>
+                                    <p><span className="font-bold">3PA:</span> 3-Point Field Goals Made-Attempted</p>
+                                    <p><span className="font-bold">AST:</span> Assists Per Game</p>
+                                    <p><span className="font-bold">BLK:</span> Blocks Per Game</p>
+                                    <p><span className="font-bold">FGA:</span> Field Goals Made-Attempted</p>
+                                </div>
+
+                                <div>
+                                    <p><span className="font-bold">FG%:</span> Field Goal Percentage</p>
+                                    <p><span className="font-bold">FT:</span> Free Throws Made-Attempted</p>
+                                    <p><span className="font-bold">FT%:</span> Free Throw Percentage</p>
+                                    <p><span className="font-bold">MIN:</span> Minutes Per Game</p>
+                                    <p><span className="font-bold">PF:</span> Fouls Per Game</p>
+                                </div>
+
+                                <div>
+                                    <p><span className="font-bold">PTS:</span> Points Per Game</p>
+                                    <p><span className="font-bold">REB:</span> Rebounds Per Game</p>
+                                    <p><span className="font-bold">STL:</span> Steals Per Game</p>
+                                    <p><span className="font-bold">TO:</span> Turnovers Per Game</p>
+                                </div>
+
                             </div>
-
-                            <div>
-                                <p><span className="font-bold">FG%:</span> Field Goal Percentage</p>
-                                <p><span className="font-bold">FT:</span> Free Throws Made-Attempted</p>
-                                <p><span className="font-bold">FT%:</span> Free Throw Percentage</p>
-                                <p><span className="font-bold">MIN:</span> Minutes Per Game</p>
-                                <p><span className="font-bold">PF:</span> Fouls Per Game</p>
-                            </div>
-
-                            <div>
-                                <p><span className="font-bold">PTS:</span> Points Per Game</p>
-                                <p><span className="font-bold">REB:</span> Rebounds Per Game</p>
-                                <p><span className="font-bold">STL:</span> Steals Per Game</p>
-                                <p><span className="font-bold">TO:</span> Turnovers Per Game</p>
-                            </div>
-
                         </div>
-                    </div>
-                </CardFooter>
-            </Card>
+                    </CardFooter>
+                </Card>
+            }
         </>
     )
 }
