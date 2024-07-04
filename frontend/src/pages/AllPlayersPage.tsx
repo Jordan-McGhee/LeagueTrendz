93afc8f93cb3cbb95340c60178b040da72d144d1
Jordan McGhee <jordanmcghee95@gmail.com>
2024-07-04 12:14:59 -0400

    Wireframe for all players page done

--- frontend/src/pages/AllPlayersPage.tsx
+++ frontend/src/pages/AllPlayersPage.tsx
@@ -1,4 +1,19 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import { useParams, Link } from "react-router-dom"
+
+// hook imports
+import { useFetch } from "../Hooks/useFetch";
+
+// type imports
+
+// ui imports
+import { Card, CardHeader, CardTitle, CardContent } from "../components/ui/card"
+import { DataTable } from "../components/ui/DataTable";
+
+// component imports
+import TeamLogo from "../components/ui/TeamLogo"
+import ErrorModal from "../components/ui/ErrorModal"
+import LoadingPage from "./LoadingPage"
 
 const AllPlayersPage = () => {
 
